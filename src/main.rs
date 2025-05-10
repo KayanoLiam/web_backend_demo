3af0e60d2e05eb355124549f7e4e92bfe786a4d4
Kayano <Kayano04@outlook.jp>
2025-05-11 01:53:48 +0900

    new main

--- src/main.rs
+++ src/main.rs
@@ -14,34 +14,33 @@ async fn main() -> std::io::Result<()> {
                 app_name: "Kayano".to_string(),
             }))
             .app_data(counter_data.clone())
+            .configure(config)
             // .app_data(web::Data::new(AppStateWithCounter {
             //     counter: Mutex::new(0),
             // }))
-            .service(
-                //这种方式是将路由分组，使用 scope 来分组,这个分组的路由是 /app/index
-                // web::scope("/app").route("/index", web::get().to(index)),
-                web::scope("/app")
-                    // .service(web::resource("/index2").to(index2))
-                    // .service(web::resource("/index").to(index))
-                    // .service(web::resource("/index3").to(index3)),
-                    .route("/index2", web::get().to(index2))
-                    .route("/index", web::get().to(index))
-                    .route("/index3", web::get().to(index3))
-                    .guard(guard::Host("users.rust-lang.org"))
-                    .route(
-                        "",
-                        web::get().to(|| async { HttpResponse::Ok().body("users site") }),
-                    ),
-            )
-            .service(
-                web::scope("/app2")
-                    .guard(guard::Host("www.rust-lang.org"))
-                    .route(
-                        "",
-                        web::to(|| async { HttpResponse::Ok().body("www site") }),
-                    ), // 注意路径是 ""，因为 scope 已经是 "/"
-            )
-            // .service(first_hello)
+            // .service(
+            //     //这种方式是将路由分组，使用 scope 来分组,这个分组的路由是 /app/index
+            //     // web::scope("/app").route("/index", web::get().to(index)),
+            //     web::scope("/app")
+            //         // .service(web::resource("/index2").to(index2))
+            //         // .service(web::resource("/index").to(index))
+            //         // .service(web::resource("/index3").to(index3)),
+            //         .route("/index2", web::get().to(index2))
+            //         .route("/index", web::get().to(index))
+            //         .route("/index3", web::get().to(index3))
+            //         .guard(guard::Host("users.rust-lang.org"))
+            //         ),
+            // )
+            // .service(
+            //     web::scope("/app2")
+            //         .guard(guard::Host("www.rust-lang.org"))
+            //         .route(
+            //             "",
+            //             web::to(|| async { HttpResponse::Ok().body("www site") }),
+            //         ), // 注意路径是 ""，因为 scope 已经是 "/"
+            // )
+            .service(first_hello)
+            .configure(config2)
             .service(echo)
             //这种方式是直接在这里定义路由
             .route("/hey", actix_web::web::get().to(manual_hello))
@@ -91,4 +90,35 @@ struct AppState {
 
 struct AppStateWithCounter {
     counter: Mutex<i32>,
+}
+//---------------------------------------接下来都是配置--------------------------------------
+//这是app的配置函数
+fn config(cfg: &mut web::ServiceConfig) {
+    cfg.service(
+        web::scope("/app")
+            .route("/index", web::get().to(index))
+            .route("/index2", web::get().to(index2))
+            .route(
+                "",
+                web::get()
+                    .guard(guard::Host("users.rust-lang.org"))
+                    .to(|| async { HttpResponse::Ok().body("users site") }),
+            ),
+    );
+}
+
+//这是app2的配置函数,与上面的配置函数是一样的
+fn config2(cfg: &mut web::ServiceConfig) {
+    cfg.service(
+        web::scope("/app2")
+            .route("/index", web::get().to(index))
+            .route("/index3", web::get().to(index3))
+            .route(
+                //测试这个守卫路由使用： curl -H "Host: www.rust-lang.org" http://127.0.0.1:8087/app2
+                "",
+                web::get()
+                    .guard(guard::Host("www.rust-lang.org"))
+                    .to(|| async { HttpResponse::Ok().body("www site") }),
+            ),
+    );
 }
