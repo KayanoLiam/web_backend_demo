a429f47969b888bed12622a3844a4321b9d20a17
Kayano <Kayano04@outlook.jp>
2025-05-12 00:16:51 +0900

    new cours

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,26 @@
 use std::sync::Mutex;
 
 use actix_web::{HttpResponse, HttpServer, Responder, guard, web};
-
+use openssl::ssl::{SslAcceptor, SslFiletype};
+use serde::Deserialize;
+use std::time::Duration;
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     let counter_data = web::Data::new(AppStateWithCounter {
         counter: Mutex::new(0),
     });
 
+    //加载ssl证书
+    let mut builder = SslAcceptor::mozilla_intermediate(openssl::ssl::SslMethod::tls()).unwrap();
+    builder
+        .set_private_key_file("key.pem", SslFiletype::PEM)
+        .expect("Failed to set private key");
+
+    builder
+        .set_certificate_chain_file("cert.pem")
+        .expect("Failed to set certificate chain");
+
+    //创建新的http服务器
     HttpServer::new(move || {
         actix_web::App::new()
             .app_data(web::Data::new(AppState {
@@ -42,10 +55,20 @@ async fn main() -> std::io::Result<()> {
             .service(first_hello)
             .configure(config2)
             .service(echo)
+            .service(path_test)
+            .service(path_test_by_struct)
+            .service(query_test)
             //这种方式是直接在这里定义路由
             .route("/hey", actix_web::web::get().to(manual_hello))
     })
-    .bind("127.0.0.1:8087")?
+    .keep_alive(Duration::from_secs(75)) //设置保持连接的时间
+    .workers(10) //设置工作线程数
+    .max_connections(100) //设置最大连接数
+    .max_connection_rate(10) //设置最大连接速率
+    .shutdown_timeout(10) //设置关闭连接的超时时间
+    .backlog(100) //设置请求队列的长度
+    .shutdown_timeout(60) //设置关闭连接的超时时间
+    .bind_openssl("127.0.0.1:8087", builder)?
     .run()
     .await
 }
@@ -83,6 +106,46 @@ async fn index3(data: web::Data<AppStateWithCounter>) -> String {
     *counter += 1;
     format!("Hello from index3! Counter: {}", counter)
 }
+
+//这个路由使用curl -k https://127.0.0.1:8087/path/32/yourname 测试，注意是https
+#[actix_web::get("/path/{user_id}/{name}")]
+async fn path_test(path: web::Path<(u32, String)>) -> Result<String, actix_web::Error> {
+    //获取路径参数
+    let (user_id, name) = path.into_inner(); //这个into_inner()方法是将路径参数转换为元组
+    Ok(format!(
+        "Hello from Path_test! User ID: {}, Name: {}",
+        user_id, name
+    ))
+}
+
+//这个函数关联了一个 UserInfo 结构体
+//这个函数使用curl -k https://127.0.0.1:8087/path2/32/yourname
+#[actix_web::get("/path2/{user_id}/{name}")]
+async fn path_test_by_struct(path: web::Path<UserInfo>) -> Result<String, actix_web::Error> {
+    //获取路径参数
+    let user_info = path.into_inner(); //这个into_inner()方法是将路径参数转换为结构体
+    Ok(format!(
+        "Hello from Path_test! User ID: {}, Name: {}",
+        user_info.user_id, user_info.name
+    ))
+}
+
+//这个函数使用curl -k https://127.0.0.1:8087/query?q=your_query&lang=your_lang
+#[actix_web::get("/query")]
+// 该异步处理函数用于处理 GET /query 路由，接收查询参数并返回响应字符串
+async fn query_test(query: web::Query<SearchQuery>) -> String {
+    // 检查查询参数中是否包含 lang 字段
+    if let Some(lang) = &query.lang {
+        // 如果包含 lang 字段，返回带有 lang 的响应字符串
+        format!("Hello from query_test! Query: {}, lang: {}", query.q, lang)
+    } else {
+        // 如果不包含 lang 字段，仅返回 q 字段
+        format!("Hello from query_test! Query: {}", query.q)
+    }
+}
+
+
+
 //--------------------------------------接下来都是结构体--------------------------------------
 struct AppState {
     app_name: String,
@@ -90,6 +153,19 @@ struct AppState {
 
 struct AppStateWithCounter {
     counter: Mutex<i32>,
+}
+
+//这个结构体是用来解析路径参数的,凡是要解析路径参数的结构体都要加上这个宏
+#[derive(Deserialize)]
+struct UserInfo {
+    user_id: u32,
+    name: String,
+}
+
+#[derive(Deserialize)]
+struct SearchQuery {
+    q: String,
+    lang:Option<String>,
 }
 //---------------------------------------接下来都是配置--------------------------------------
 //这是app的配置函数
